c88fb5c2e23248038d671e4eefd6bb37f388e046
smuely <info@yannickmueller.dev>
2024-08-21 15:27:26 +0200

    added some test routes for link shortener

--- pages/r/[id].vue
+++ pages/r/[id].vue
@@ -1,7 +1,14 @@
 <template>
   <div>ID: {{ route.params.id }}</div>
+  <p>
+    goes to:
+    {{ link }}
+  </p>
 </template>
 <script setup lang="ts">
 const route = useRoute();
+const router = useRouter();
 definePageMeta({ name: "ignore" });
+const link = links.filter((link) => link.id === route.params.id)[0].link;
+if (process.client) window.location.href = link;
 </script>
--- utils/themes.ts
+++ utils/themes.ts
@@ -1,1 +1,12 @@
-export default ["light", "dark", "black", "forest", "dracula", "sunset", "aqua", "bumblebee", "cyberpunk"]
+export default [
+  "light",
+  "dark",
+  "black",
+  "forest",
+  "dracula",
+  "sunset",
+  "aqua",
+  "bumblebee",
+  "cyberpunk",
+];
+
