022392c1cdd885e884e0caaba863c766777a5a97
smuely <info@yannickmueller.dev>
2024-08-28 11:49:56 +0200

    added custom theme ysm

--- components/ThemeChanger.vue
+++ components/ThemeChanger.vue
@@ -1,41 +1,23 @@
 <template>
-  <div class="dropdown mb-2">
-    <div tabindex="0" role="button" class="btn mt-3 mr-3 mb-2">
-      Theme
-      <svg
-        width="12px"
-        height="12px"
-        class="inline-block h-2 w-2 fill-current opacity-60"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 2048 2048"
-      >
-        <path
-          d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"
-        ></path>
-      </svg>
-    </div>
-    <ul
-      tabindex="0"
-      class="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl right-0"
-    >
-      <li>
-        <input
-          type="radio"
-          name="theme-dropdown"
-          class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-          aria-label="default"
-          value="default"
-        />
-      </li>
-      <li v-for="theme of themes">
-        <input
-          type="radio"
-          name="theme-dropdown"
-          class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-          :aria-label="theme"
-          :value="theme"
-        />
-      </li>
-    </ul>
-  </div>
+	<div class="dropdown mb-2">
+		<div tabindex="0" role="button" class="btn mt-3 mr-3 mb-2">
+			Theme
+			<svg width="12px" height="12px" class="inline-block h-2 w-2 fill-current opacity-60"
+				xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048">
+				<path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+			</svg>
+		</div>
+		<ul tabindex="0" class="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl right-0">
+			<li>
+				<input type="radio" name="theme-dropdown"
+					class="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Default"
+					value="default" />
+			</li>
+			<li v-for="theme of themeNames">
+				<input type="radio" name="theme-dropdown"
+					class="theme-controller btn btn-sm btn-block btn-ghost justify-start" :aria-label="theme"
+					:value="theme.toLowerCase()" />
+			</li>
+		</ul>
+	</div>
 </template>
